724c4362f780fe2e27cbe8f6297c2cf84673deb3
MustufaMemon10 <mustufasam06@gmail.com>
2024-07-18 14:57:00 +0530

    using stackindex and changed bottomnav bar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,6 +10,7 @@ import 'package:hiremi_version_two/Custom_Widget/drawer_child.dart';
 import 'package:hiremi_version_two/Custom_Widget/verification_status.dart';
 import 'package:hiremi_version_two/InternshipScreen.dart';
 import 'package:hiremi_version_two/Notofication_screen.dart';
+import 'package:hiremi_version_two/Utils/AppSizes.dart';
 import 'package:hiremi_version_two/Utils/colors.dart';
 import 'package:hiremi_version_two/experienced_jobs.dart';
 import 'package:hiremi_version_two/fresherJobs.dart';
@@ -92,25 +93,24 @@ class _HomePageState extends ConsumerState<HomePage> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        // leading: Padding(
-        // padding: EdgeInsets.all(screenWidth*0.02),
-        // child: GestureDetector(
-        //   onTap: () {
-        //     Scaffold.of(context).openDrawer();
-        //   },
-        //   child: Container(
-        //       padding: EdgeInsets.all(Sizes.responsiveSm(context)),
-        //       decoration: BoxDecoration(
-        //         borderRadius: BorderRadius.circular(8.0),
-        //         color: AppColors.bgBlue,
-        //       ),
-        //       child: const Icon(
-        //         Icons.notes_rounded,
-        //         size: 22,
-        //       )),
-        // )
-        // ),
-
+        leading: Padding(
+        padding: EdgeInsets.all(screenWidth*0.02),
+        child: GestureDetector(
+          onTap: () {
+            Scaffold.of(context).openDrawer();
+          },
+          child: Container(
+              padding: EdgeInsets.all(Sizes.responsiveSm(context)),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: AppColors.bgBlue,
+              ),
+              child: const Icon(
+                Icons.notes_rounded,
+                size: 22,
+              )),
+        )
+        ),
         title: const Text(
           "Hiremi's Home",
           style: TextStyle(),
