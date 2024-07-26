238bb4c098916826350fc0b7454892d1e5df804b
MustufaMemon10 <mustufasam06@gmail.com>
2024-07-26 14:17:05 +0530

    - appbar in bottomnavigationbar.dart

--- lib/bottomnavigationbar.dart
+++ lib/bottomnavigationbar.dart
@@ -1,12 +1,15 @@
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hiremi_version_two/Custom_Widget/Custom_alert_box.dart';
+import 'package:hiremi_version_two/Custom_Widget/drawer_child.dart';
 import 'package:hiremi_version_two/HomePage.dart';
 import 'package:hiremi_version_two/Screens/Profile_Screen/Profile_Screen.dart';
 import 'package:hiremi_version_two/applies_screen.dart';
 import 'package:hiremi_version_two/providers/verified_provider.dart';
 import 'package:hiremi_version_two/queries_screen.dart';
+import 'package:hiremi_version_two/screens/Profile_Screen/sections/widgets_mustufa/appbar/AppBar.dart';
 import 'package:hiremi_version_two/verified_popup.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -33,20 +36,15 @@ class _NewNavbarState extends ConsumerState<NewNavbar> {
   @override
   void initState() {
     super.initState();
-    
     _checkFirstVerification();
-    
   }
-
-  
-  
 
   Future<void> _checkFirstVerification() async {
     final prefs = await SharedPreferences.getInstance();
     // final bool isVerified = ref.read(verificationProvider);
     final bool isFirstVerification =
         prefs.getBool('isFirstVerification') ?? true;
-        final isVerified = ref.watch(verificationProvider);
+    final isVerified = ref.watch(verificationProvider);
     if (isVerified && isFirstVerification) {
       _showVerificationPopup();
       await prefs.setBool('isFirstVerification', false);
@@ -94,81 +92,95 @@ class _NewNavbarState extends ConsumerState<NewNavbar> {
     );
   }
 
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<String> _titles = [
+    'Hiremi\'s Home',
+    'Applies',
+    'Queries',
+    'Profile',
+  ];
   @override
   Widget build(BuildContext context) {
     final isVerified = ref.watch(verificationProvider);
     return Scaffold(
+      key: scaffoldKey,
       resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.white,
-        body: _pages[widget.initTabIndex],
-        bottomNavigationBar: Container(
-          height: MediaQuery.sizeOf(context).height * 0.08,
-          decoration: BoxDecoration(
-              color: AppColors.primary,
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(16),
-                bottomLeft: Radius.circular(32),
-                bottomRight: Radius.circular(32),
-              ),
-              boxShadow: const [
-                BoxShadow(
-                    color: Colors.black38, blurRadius: 10, offset: Offset(4, 4))
-              ]),
-          child: BottomAppBar(
-            color: Colors.white,
-            shape: const CircularNotchedRectangle(),
-            notchMargin: 15,
-            child: Padding(
-              padding: EdgeInsets.all(Sizes.responsiveXxs(context)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildNavItem(Icons.home_filled, 'HOME', 0),
-                  _buildNavItem(Icons.list_alt_rounded, 'APPLIES', 1),
-                  SizedBox(width: Sizes.responsiveXxl(context)),
-                  _buildNavItem(Icons.local_activity_outlined, 'QUERIES', 2),
-                  _buildNavItem(Icons.person_outline, 'PROFILE', 3),
-                ],
-              ),
+      backgroundColor: Colors.white,
+      appBar: SAppbar(
+        title: _titles[widget.initTabIndex],
+        scaffoldKey: scaffoldKey,
+      ),
+      drawer: const Drawer(
+        child: DrawerChild(),
+      ),
+      body: _pages[widget.initTabIndex],
+      bottomNavigationBar: Container(
+        height: MediaQuery.sizeOf(context).height * 0.08,
+        decoration: BoxDecoration(
+            color: AppColors.primary,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(16),
+              topRight: Radius.circular(16),
+              bottomLeft: Radius.circular(32),
+              bottomRight: Radius.circular(32),
+            ),
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.black38, blurRadius: 10, offset: Offset(4, 4))
+            ]),
+        child: BottomAppBar(
+          color: Colors.white,
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 15,
+          child: Padding(
+            padding: EdgeInsets.all(Sizes.responsiveXxs(context)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildNavItem(Icons.home_filled, 'HOME', 0),
+                _buildNavItem(Icons.list_alt_rounded, 'APPLIES', 1),
+                SizedBox(width: Sizes.responsiveXxl(context)),
+                _buildNavItem(Icons.local_activity_outlined, 'QUERIES', 2),
+                _buildNavItem(Icons.person_outline, 'PROFILE', 3),
+              ],
             ),
           ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: Transform.scale(
-          scale: pi * 0.4,
-          child: FloatingActionButton(
-              onPressed: () {
-                if (!isVerified) {
-                  _showPopUp();
-                }
-              },
-              elevation: 4,
-              backgroundColor: Colors.white,
-              shape: const CircleBorder(),
-              child: const Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.all_inclusive,
-                      color: Color(0xFFC1272D),
-                      size: 20,
-                    ),
-                    Text(
-                      'HIREMI',
-                      style:
-                          TextStyle(fontSize: 7, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '360',
-                      style: TextStyle(fontSize: 6, color: Color(0xFFC1272D)),
-                    ),
-                  ],
-                ),
-              )),
-        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Transform.scale(
+        scale: pi * 0.4,
+        child: FloatingActionButton(
+            onPressed: () {
+              if (!isVerified) {
+                _showPopUp();
+              }
+            },
+            elevation: 4,
+            backgroundColor: Colors.white,
+            shape: const CircleBorder(),
+            child: const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.all_inclusive,
+                    color: Color(0xFFC1272D),
+                    size: 20,
+                  ),
+                  Text(
+                    'HIREMI',
+                    style: TextStyle(fontSize: 7, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '360',
+                    style: TextStyle(fontSize: 6, color: Color(0xFFC1272D)),
+                  ),
+                ],
+              ),
+            )),
+      ),
     );
   }
 
