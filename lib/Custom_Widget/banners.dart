a4877347811e990d8fd164e679b36552405d7938
MustufaMemon10 <mustufasam06@gmail.com>
2024-07-29 13:58:55 +0530

    redirecting on banner implemented

--- lib/Custom_Widget/banners.dart
+++ lib/Custom_Widget/banners.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hiremi_version_two/bottomnavigationbar.dart';
 import 'package:hiremi_version_two/verify.dart';
 
 class AdBanner extends StatelessWidget {
@@ -16,14 +17,29 @@ class AdBanner extends StatelessWidget {
       width: double.infinity,
       child: GestureDetector(
         onTap: () {
-          !isVerified
-              ? Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          const VerificationScreen()))
-              : null;
-
+          if(!isVerified) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                    const VerificationScreen()));
+          }
+          if(isVerified) {
+            switch (image) {
+              case 'images/icons/Hiremi Verified Banner2.png':
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            NewNavbar(initTabIndex: 1,)));
+                break;
+              case 'images/icons/Hiremi Verified Banner3.png':
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            NewNavbar(initTabIndex: 2,)));
+                break;
+            }
+          }
           ///  TODO :Navigate to Mentor Screen
         },
         child: ClipRRect(
